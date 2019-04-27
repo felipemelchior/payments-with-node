db15b18231f9229e4a0e1055d869fa487a9bc453
Felipe Homrich Melchior <fehmel@gmail.com>
2019-04-27 16:53:24 -0300

    feat: add function in controller

--- angular/app/billingCycle/billingCycleController.js
+++ angular/app/billingCycle/billingCycleController.js
@@ -59,6 +59,34 @@
             })
         }
 
+        vm.addCredit = (index) => {
+            vm.billingCycle.credits.splice(index + 1, 0, {})
+        }
+
+        vm.cloneCredit = (index, {name, value}) => {
+            vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+        }
+
+        vm.deleteCredit = (index) => {
+            if(vm.billingCycle.credits.length > 1) {
+                vm.billingCycle.credits.splice(index, 1)
+            }
+        }
+
+        vm.addDebt = (index) => {
+            vm.billingCycle.debts.splice(index + 1, 0, {})
+        }
+
+        vm.cloneDebt = (index, {name, value, status}) => {
+            vm.billingCycle.debts.splice(index + 1, 0, {name, value, status})
+        }
+
+        vm.deleteCredit = (index) => {
+            if(vm.billingCycle.debts.length > 1) {
+                vm.billingCycle.debts.splice(index, 1)
+            }
+        }
+
         vm.refresh()
     }
 })()

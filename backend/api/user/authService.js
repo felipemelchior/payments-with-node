b7525bae9e3a3a8e6ff93db9b515e9947c4ad5ab
Felipe Homrich Melchior <fehmel@gmail.com>
2019-05-30 14:08:30 -0300

    feat: finish function auth

--- backend/api/user/authService.js
+++ backend/api/user/authService.js
@@ -31,3 +31,51 @@ const login = (req, res, next) => {
         }
     })
 }
+
+const validateToken = (req, res, next) => {
+    const token = req.body.token || ''
+    jwt.verify(token, env.authSecret, (err, decoded) => {
+        return res.status(200).send({valid: !err})
+    })
+}
+
+const signup = (req, res, next) => {
+    const name = req.body.name || ''
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    const confirmPassword = req.body.confirm_password || ''
+
+    if(!email.match(emailRegex)) {
+        return res.status(400).send({errors: ['O e-mail informado não é válido']})
+    }
+
+    if(!password.match(passwordRegex)) {
+        return res.status(400).send({errors: ['Senha invalida']})
+    }
+
+    const salt = bcrypt.genSaltSync()
+    const passwordHash = bcrypt.hashSync(password, salt)
+
+    if(!bcrypt.compareSync(confirmPassword, passwordHash)) {
+        return res.status(400).send({errors: ['Senhas não conferem']})
+    }
+
+    User.findOne({email}, (err, user) => {
+        if(err) {
+            return sendErrorsFromDb(res, err)
+        } else if(user) {
+            return res.status(400).send({errors: ['Usuario já cadastrado']})
+        } else {
+            const newUser = new User({ name, email, password: passwordHash })
+            newUser.save(err => {
+                if(err) {
+                    return sendErrorsFromDb(res,err)
+                } else {
+                    login(req, res, next)
+                }
+            })
+        }
+    })
+}
+
+module.exports = { login, signup, validateToken }
